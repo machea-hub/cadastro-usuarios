abb24fac5fdcaabc2e0064fd83a9d9ac4e9cc94c
machea-hub <lucas.machea@hotmail.com>
2025-09-19 19:17:44 -0300

    style

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,39 @@
 */
 import{ Container, Title, TopBackground, Form, ContainerInputs, Input, Button, InputLabel } from './styles'
 
+import UsersImage from "./assets/users.png"
+
 function Home() {  
   return (
   
       <Container>
         <TopBackground>
-          <img/>
+          <img src={UsersImage} alt="Usuários" />
         </TopBackground>
 
         <Form>
         <Title>Cadastrar Usuário</Title>
 
         <ContainerInputs>
-          <div>
             <div>
               <InputLabel>
-                Nome<span>*</span>
+                Nome <span>*</span>
               </InputLabel>
               <Input type='text' placeholder='Nome do Usuário' />
             </div>
             <div>
               <InputLabel>
-                Idade<span>*</span>
+                Idade <span>*</span>
               </InputLabel>
               <Input type='number' placeholder='Idade do Usuário' />
             </div>
-          </div>
-          <div>
-            <InputLabel>
-              E-mail<span>*</span>
-            </InputLabel>
-            <Input type='email' placeholder='E-mail do Usuário' />
-          </div>
         </ContainerInputs>
+        <div style={{ width: '100%' }}>
+          <InputLabel>
+            E-mail <span>*</span>
+          </InputLabel>
+          <Input type='email' placeholder='E-mail do Usuário' />
+        </div>
         <Button>Cadastrar Usuário</Button>
 
         </Form>
--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,94 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``
+export const Container = styled.div`
+    background: #181f36;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 20px;
+    height: 100vh;
+    flex-direction: column;
+`
 
-export const TopBackground = styled.div``
+export const TopBackground = styled.div`
+    background: linear-gradient(to right, #fe7e5d, #7f3841);
+    height: 30vh;
+    width: 90vw;
+    max-width: 800px;
+    border-radius: 30px;
 
-export const Form = styled.form``
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-export const ContainerInputs = styled.div``
+    img {
+        max-width: 100%;
+        height: 100%;
+    }
+`
 
-export const InputLabel = styled.label``
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    max-width: 500px;
+`
 
-export const Input = styled.input``
+export const Title = styled.h2`
+    color: #fff;
+    text-align: center;
+    font-size: 38px;
+    font-style: normal;
+    font-weight: 600;
+`
 
-export const Button = styled.button``
+export const ContainerInputs = styled.div`
+    display: flex;
+    gap: 20px;
+`
 
-export const Title = styled.h2``
+export const InputLabel = styled.label`
+    color: #fff;
+    font-size: 12px;
+    font-weight: 500;
+
+    span {
+        color: #ef4f45;
+        font-weight: bold;
+    }
+`
+
+export const Input = styled.input`
+    border-radius: 10px;
+    border: 1px solid #d2dae2;
+    background-color: #fff;
+    padding: 12px 20px;
+    outline: none;
+    width: 100%;
+
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none !important;
+        margin: 0 !important;
+    }
+`
+
+export const Button = styled.button`
+    border: none;
+    background: linear-gradient(180deg, #fe7e5d 0%, #7f3841 100%);
+    font-size: 16px;
+    color: #fff;
+    padding: 16px 32px;
+    width: fit-content;
+    cursor: pointer;
+    border-radius: 30px;
+
+    &:hover {
+        opacity: 0.8;   
+    }
+
+    &:active {
+        opacity: 0.5;
+    }
+`
+
