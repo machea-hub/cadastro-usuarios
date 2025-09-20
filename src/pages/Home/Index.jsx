3e943fe9d177a17a41884e643b159c6cc6c76bbf
machea-hub <lucas.machea@hotmail.com>
2025-09-20 00:18:53 -0300

    .

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -2,11 +2,23 @@
   Exportar "Padrão" -> Uma coisa só por pagina
   apenas exportar
 */
+import { use, useRef } from 'react'
+
 import{ Container, Title, TopBackground, Form, ContainerInputs, Input, Button, InputLabel } from './styles'
 
 import UsersImage from "../../assets/users.png"
 
 function Home() {  
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser() {
+    console.log(inputName.current.value);
+    
+  }
+
   return (
   
       <Container>
@@ -22,22 +34,22 @@ function Home() {
               <InputLabel>
                 Nome <span>*</span>
               </InputLabel>
-              <Input type='text' placeholder='Nome do Usuário' />
+              <Input type='text' placeholder='Nome do Usuário' ref={inputName}/>
             </div>
             <div>
               <InputLabel>
                 Idade <span>*</span>
               </InputLabel>
-              <Input type='number' placeholder='Idade do Usuário' />
+              <Input type='number' placeholder='Idade do Usuário' ref={inputAge} />
             </div>
         </ContainerInputs>
         <div style={{ width: '100%' }}>
           <InputLabel>
             E-mail <span>*</span>
           </InputLabel>
-          <Input type='email' placeholder='E-mail do Usuário' />
+          <Input type='email' placeholder='E-mail do Usuário' ref={inputEmail} />
         </div>
-        <Button>Cadastrar Usuário</Button>
+        <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
 
         </Form>
       </Container>
