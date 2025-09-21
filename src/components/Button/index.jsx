55b5c39d6e7bee7e156bf5e7beff1a4cc882ed46
machea-hub <lucas.machea@hotmail.com>
2025-09-21 20:32:45 -0300

    PropType

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { Button } from './styles'
 
 function DefaultButton({ children, ...rest }) {
@@ -6,6 +7,10 @@ function DefaultButton({ children, ...rest }) {
         <Button {...rest}>{children}</Button>
     )
     
+}
+
+DefaultButton.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default DefaultButton
