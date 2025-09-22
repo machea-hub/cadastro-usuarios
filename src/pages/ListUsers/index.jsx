aa40cf21b1f4c9478e7a9ad26a04d4f0a97ea84c
machea-hub <lucas.machea@hotmail.com>
2025-09-22 01:01:18 -0300

    api useEffect

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react"
+import api from "../../services/api"
 import Button from '../../components/Button'
 import TopBackground from '../../components/TopBackground'
 
 function ListUsers() {
+
+    useEffect(() => {
+        async function getUsers() {
+            const usersFromApi = await api.get("/usuarios")
+            console.log(usersFromApi);
+            
+        }
+        getUsers()
+    }, [])
+    
 
     return (
         <div>
